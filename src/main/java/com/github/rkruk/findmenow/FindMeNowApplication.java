e7ad196f0bdce2d8ed11531b1155b01809eda540
Łukasz Dębicki <sacull@gmail.com>
2019-11-21 18:37:35 +0100

    Add first files for initial commit

---
+++ src/main/java/com/github/rkruk/findmenow/FindMeNowApplication.java
@@ -0,0 +1,10 @@
+package com.github.rkruk.findmenow;
+
+import org.springframework.boot.SpringApplication;
+
+public class FindMeNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindMeNowApplication.class, args);
+    }
+}
