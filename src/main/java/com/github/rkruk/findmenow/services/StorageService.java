5410b6330207c3a895ac091ae7cf73e9b65609ee
Łukasz Dębicki <sacull@gmail.com>
2019-11-22 11:49:39 +0100

    Add implementation for storage method

--- src/main/java/com/github/rkruk/findmenow/services/StorageService.java
+++ src/main/java/com/github/rkruk/findmenow/services/StorageService.java
@@ -3,11 +3,24 @@ package com.github.rkruk.findmenow.services;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Service
 public class StorageService {
 
-    public boolean store(MultipartFile file) {
+    private static String STORAGE_FOLDER = "c:\\temp\\";
 
-        return false;
+    public boolean store(MultipartFile file) {
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(STORAGE_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
     }
 }
