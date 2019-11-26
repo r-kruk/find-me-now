f5154356dbec7245bb26005d21b09033e8f4a085
Rafal Kruk <kruku@o2.pl>
2019-11-26 23:38:17 +0100

    Modify Class UserPanelPageController; modify file user-panel.jsp

--- src/main/java/com/github/rkruk/findmenow/controllers/UserPanelPageController.java
+++ src/main/java/com/github/rkruk/findmenow/controllers/UserPanelPageController.java
@@ -1,15 +1,30 @@
 package com.github.rkruk.findmenow.controllers;
 
+import com.github.rkruk.findmenow.models.User;
+import com.github.rkruk.findmenow.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user-panel")
 public class UserPanelPageController {
 
+    private final UserRepository userRepository;
+
+    public UserPanelPageController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping
     public String showUserPanelPage() {
         return "/WEB-INF/views/user-panel.jsp";
     }
+
+//    @GetMapping
+//    public User getUser(@PathVariable Long id) {
+//        User one = userRepository.getOne(id);
+//        return one;
+//    }
 }
