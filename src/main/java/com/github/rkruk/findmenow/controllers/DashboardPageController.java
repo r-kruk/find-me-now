efc2f6a8597e00a76a0c11b6ef4b878c8116a741
Rafal Kruk <kruku@o2.pl>
2019-12-02 17:47:48 +0100

    Indicate if user has place

--- src/main/java/com/github/rkruk/findmenow/controllers/DashboardPageController.java
+++ src/main/java/com/github/rkruk/findmenow/controllers/DashboardPageController.java
@@ -1,8 +1,11 @@
 package com.github.rkruk.findmenow.controllers;
 
 import com.github.rkruk.findmenow.daos.SchemeDAO;
+import com.github.rkruk.findmenow.daos.UserDAO;
 import com.github.rkruk.findmenow.models.User;
+import com.github.rkruk.findmenow.repositories.UserRepository;
 import com.github.rkruk.findmenow.services.SchemeService;
+import com.github.rkruk.findmenow.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,11 +20,13 @@ import java.util.List;
 @RequestMapping("/")
 public class DashboardPageController {
 
+    private UserService userService;
     private SchemeService schemeService;
 
     @Autowired
-    public DashboardPageController(SchemeService schemeService) {
+    public DashboardPageController(SchemeService schemeService, UserService userService) {
         this.schemeService = schemeService;
+        this.userService = userService;
     }
 
     @GetMapping
@@ -35,7 +40,8 @@ public class DashboardPageController {
 
     @PostMapping
     public String userToBeFound(String search, Long id) {
-        return "redirect:/?id=" + id;
+        Long placeId = userService.getPlaceIdOfSearchedUser(search);
+        return "redirect:/?id=" + placeId;
     }
 
 }
