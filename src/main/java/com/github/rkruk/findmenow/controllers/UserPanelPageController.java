2b12b71ecb624aafbf504b2cf1633f4d904c6312
Łukasz Dębicki <sacull@gmail.com>
2019-12-03 15:53:40 +0100

    Make base for taking place by user

--- src/main/java/com/github/rkruk/findmenow/controllers/UserPanelPageController.java
+++ src/main/java/com/github/rkruk/findmenow/controllers/UserPanelPageController.java
@@ -1,10 +1,15 @@
 package com.github.rkruk.findmenow.controllers;
 
+import com.github.rkruk.findmenow.dtos.PlaceDTO;
+import com.github.rkruk.findmenow.dtos.SchemeDTO;
 import com.github.rkruk.findmenow.dtos.UserDTO;
+import com.github.rkruk.findmenow.services.PlaceService;
+import com.github.rkruk.findmenow.services.SchemeService;
 import com.github.rkruk.findmenow.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,8 +20,13 @@ import java.security.Principal;
 public class UserPanelPageController {
 
     private final UserService userService;
-    public UserPanelPageController(UserService userService) {
+    private PlaceService placeService;
+    private SchemeService schemeService;
+
+    public UserPanelPageController(UserService userService, PlaceService placeService, SchemeService schemeService) {
         this.userService = userService;
+        this.placeService = placeService;
+        this.schemeService = schemeService;
     }
 
     @GetMapping
@@ -26,8 +36,18 @@ public class UserPanelPageController {
         String username = principal.getName();
         Long id = userService.getIdOfLoggedUser(username);
         UserDTO userDTO = userService.getOne(id);
+        PlaceDTO placeDTO = null;
+        SchemeDTO schemeDTO = null;
+        if (userDTO.getPlaceId() != null) {
+            placeDTO = placeService.getPlaceDTOById(userDTO.getPlaceId());
+        }
+        if (placeDTO != null && placeDTO.getSchemeId() != null) {
+            schemeDTO = schemeService.getSchemeDTOById(placeDTO.getSchemeId());
+        }
         model.addAttribute("userDTO", userDTO);
         model.addAttribute("activeTab", activeTab);
+        model.addAttribute("placeDTO", placeDTO);
+        model.addAttribute("schemeDTO", schemeDTO);
         return "/WEB-INF/views/user-panel.jsp";
     }
 
@@ -36,14 +56,24 @@ public class UserPanelPageController {
                                  Long id) {
         String username = principal.getName();
         Long loggedUserId = userService.getIdOfLoggedUser(username);
-        if (loggedUserId == id) {
+        if (loggedUserId.equals(id)) {
             userService.deactivateUser(id);
-
         }
         else {
             return "redirect:/";
         }
         return "redirect:/logout";
+    }
 
+    @GetMapping("/take-place")
+    public String showTakePlacePage() {
+        // TODO: 03.12.2019 Create JSP page ("take-place.jsp" will be good)
+        return null;
+    }
+
+    @PostMapping("/take-place")
+    public String takePlace() {
+        // TODO: 03.12.2019 Make user able to take place
+        return null;
     }
 }
